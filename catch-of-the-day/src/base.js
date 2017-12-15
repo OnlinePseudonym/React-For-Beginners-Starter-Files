ffa0df311aac3f3710999127c7c215c2ad35aec8
Kevin Reynolds <Kevin.T.Reynolds30@gmail.com>
2017-12-15 13:40:29 -0700

    add persisting state firebase

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyBuAoazOq-SOyHM3PhRnhmXYQhmNTARObk",
+    authDomain: "catch-of-the-day-kevin-98b73.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-kevin-98b73.firebaseio.com",
+});
+
+export default base;
